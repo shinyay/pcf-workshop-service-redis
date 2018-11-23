bfb0a1a804174ee5e5b5439440f67e0baf419ed7
shinyay <shinya.com@gmail.com>
2018-11-23 22:46:02 +0900

    Add cacheable massage meathod
    The meathoad has cacheable annotation

--- src/main/java/com/example/hellopcf/HelloPcfRedisApplication.java
+++ src/main/java/com/example/hellopcf/HelloPcfRedisApplication.java
@@ -2,11 +2,27 @@ package com.example.hellopcf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
 
 @SpringBootApplication
+@RestController
+@EnableCaching
 public class HelloPcfRedisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloPcfRedisApplication.class, args);
+    }
+}
+
+@Component
+class Greeter {
+    @Cacheable("hello")
+    public String hello() {
+        return "Hello, Redis. It's " + OffsetDateTime.now() + " now.";
     }
 }
