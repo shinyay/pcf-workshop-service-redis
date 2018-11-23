912092b8eb20f6ec4ad5136db201331e03adc82c
shinyay <shinya.com@gmail.com>
2018-11-23 22:50:42 +0900

    Add methoad call cacheable message meathod by Get Meathod
    The meathod has GetMapping annotation

--- src/main/java/com/example/hellopcf/HelloPcfRedisApplication.java
+++ src/main/java/com/example/hellopcf/HelloPcfRedisApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.OffsetDateTime;
@@ -13,6 +14,16 @@ import java.time.OffsetDateTime;
 @RestController
 @EnableCaching
 public class HelloPcfRedisApplication {
+    private final Greeter greeter;
+
+    public HelloPcfRedisApplication(Greeter greeter) {
+        this.greeter = greeter;
+    }
+
+    @GetMapping("/")
+    String hello() {
+        return greeter.hello();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HelloPcfRedisApplication.class, args);
