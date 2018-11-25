7f954227184967d3ff7a52ceabb675d8e83ed0ec
shinyay <shinya.com@gmail.com>
2018-11-25 14:14:42 +0900

    Modify package configuration

--- src/main/java/com/example/hellopcf/HelloPcfRedisApplication.java
+++ src/main/java/com/example/hellopcf/HelloPcfRedisApplication.java
@@ -1,3 +1,5 @@
+package com.example.hellopcf;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.Cacheable;
